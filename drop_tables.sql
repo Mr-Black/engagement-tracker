c5d304de95eb98360dcdf8fd6bc1a45dd1ad085d
Mike Barnes <mr-black@mr-black-ultrabook.(none)>
2013-12-02 00:34:17 -0500

    Initial commit of sql files, thrift file, and eclipse files

---
+++ drop_tables.sql
@@ -0,0 +1,1 @@
+DROP TABLE IF EXISTS Users, EegPower, EegAttention, EegRaw, HeartRate; 
---
+++ make_tables.sql
@@ -0,0 +1,33 @@
+CREATE TABLE IF NOT EXISTS Users (
+	google_id VARCHAR(100) PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS EegPower (
+	id serial PRIMARY KEY,
+	google_id VARCHAR(100) references users(google_id),
+	timestamp TIMESTAMP WITH TIME ZONE,
+	alpha INT,
+	beta INT,
+	theta INT
+);
+
+CREATE TABLE IF NOT EXISTS EegAttention (
+	id serial PRIMARY KEY,
+	google_id VARCHAR(100) references users(google_id),
+	timestamp TIMESTAMP WITH TIME ZONE,
+	attention INT
+);
+
+CREATE TABLE IF NOT EXISTS EegRaw (
+	id serial PRIMARY KEY,
+	google_id VARCHAR(100) references users(google_id),
+	timestamp TIMESTAMP WITH TIME ZONE,
+	raw INT
+);
+
+CREATE TABLE IF NOT EXISTS HeartRate (
+	id serial PRIMARY KEY,
+	google_id VARCHAR(100) references users(google_id),
+	timestamp TIMESTAMP WITH TIME ZONE,
+	bpm INT
+);
